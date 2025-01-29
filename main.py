b1b6cee50846f5a31a491a32b584c1097c398b8a
Ronnie95 <t_daron@yahoo.com>
2025-01-29 11:18:57 -0500

    post method

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
 from fastapi import FastAPI
-
+from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+    brand: Optional[str] = None
 
 
 inventory = {
@@ -23,3 +30,7 @@ def about():
 @app.get("/get-item/{item_id}")
 def get_item(item_id: int):
     return inventory[item_id]
+
+@app.post("/create-item")
+def create_iten(item: Item):
+    return {}
