0380400c58f88945e6b63786f0b1721cc38fb832
Ronnie95 <t_daron@yahoo.com>
2025-01-29 11:29:27 -0500

    updates

--- main.py
+++ main.py
@@ -31,6 +31,10 @@ def about():
 def get_item(item_id: int):
     return inventory[item_id]
 
-@app.post("/create-item")
-def create_iten(item: Item):
-    return {}
+@app.post("/create-item/{item_id}")
+def create_iten(item_id: int, item: Item):
+    if item_id in inventory:
+        return {"Error": "Item ID already exists"}
+
+    inventory[item_id] = item
+    return inventory[item_id]
