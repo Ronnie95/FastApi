01a0671106295d41232884fae6325ed56bfa3122
Ronnie95 <t_daron@yahoo.com>
2025-01-30 21:52:36 -0500

    database creation

--- __pycache__/TO Do/todo.py
+++ __pycache__/TO Do/todo.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+from sqlmodel import Field, Session, SQLModel, create_engine, select
+
+
+class ToDo(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    name: str = Field(index=True)
+    task: str = Field(index=True)
+
+
+
 
 app = FastAPI()
+
 
 @app.get("/")
 def home():
